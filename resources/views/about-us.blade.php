14f716b0e76cc430e3db58cd5adb5602089d84b0
Gohelboy <dwarkeshgohel1@gmail.com>
2022-11-14 21:38:30 +0530

    first commit

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Test</title>
+        @extends('base')
+    </head>
+    <body>
+        @section('content')
+        <div class="about-box">
+            <div class="about-card">
+                <div class="image"><img src="{{ asset('images/parth.png') }}" /></div>
+                <div class="desc">
+                    <h5>PARTH PANDYA</h5>
+                    <p>Website Designer, Project Manager</p>
+                </div>
+            </div>
+            <div class="about-card">
+                <div class="image"><img src="{{ asset('images/gohel.png') }}" /></div>
+                <div class="desc">
+                    <h5>DWARKESH GOHEL</h5>
+                    <p>Website Designer, Frontend Devloper, Backand Devloper, Project Manager</p>
+                </div>
+            </div>
+            <div class="about-card">
+                <div class="image"><img src="{{ asset('images/hems.png') }}" /></div>
+                <div class="desc">
+                    <h5>HIMANSHU DABHI</h5>
+                    <p>Frontend Devloper, Backand Devloper</p>
+                </div>
+            </div>
+        </div>
+        @stop
+    </body>
+</html>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,3 @@
+<h1 align="center">header</h1>
+@yield('content')
+<h1 align="center">Footer</h1>
